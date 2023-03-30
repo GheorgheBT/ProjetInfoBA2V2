93c8a48116ad393bfc13187a83d3d617a33acdf2
GENIAL <117689214+Ssg243@users.noreply.github.com>
2023-03-30 18:46:01 +0200

    .

--- app/src/main/java/com/example/projetinfoba2/Balle.kt
+++ app/src/main/java/com/example/projetinfoba2/Balle.kt
@@ -7,18 +7,36 @@ import android.graphics.Canvas
 import android.graphics.RectF
 
 class Balle(private val context: Context, private val x: Float, private val y: Float) {
-
+    private val balleModele = intArrayOf(R.drawable.ball) // liste des images des balles
     private val balleImage: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.ball) // recupere l'image de la balle
     private val scaledBalleImage: Bitmap = Bitmap.createScaledBitmap(balleImage, 90, 45, true) // adapte les dimension de la balle
     private val ballePosition = RectF(x, y, x + scaledBalleImage.width, y + scaledBalleImage.height) // encode la position de la balle dans un rectangle
     private val balleVitesse = 20f // la vitesse eb x à laquelle la balle va se deplacer
+    private val balleDegats = 100
+    private var balleOnScreen : Boolean = true
+
+
+
 
     fun draw(canvas: Canvas) {
-        canvas.drawBitmap(scaledBalleImage, null, ballePosition, null) //dessine la balle dans le rectangle defini plus haut
+        if (balleOnScreen) {//dessine la balle dans le rectangle defini plus haut
+            canvas.drawBitmap(
+                scaledBalleImage,
+                null,
+                ballePosition,
+                null
+            )
+        }
+
     }
 
-    fun updatePosition() { // actualise la position de la balle selon sa vitesse 
-        ballePosition.left += balleVitesse
-        ballePosition.right += balleVitesse
+    fun updatePosition() { // actualise la position de la balle selon sa vitesse
+        if (balleOnScreen) {
+            ballePosition.left += balleVitesse
+            ballePosition.right += balleVitesse
+        }
     }
+     fun ballDestroyed(){
+         balleOnScreen = false
+     }
 }
