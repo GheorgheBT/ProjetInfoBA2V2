dc2b25b5c86f0accce729dbe561f82ee5bb6c5a7
GENIAL <117689214+Ssg243@users.noreply.github.com>
2023-04-02 20:11:40 +0200

    .

--- app/src/main/java/com/example/projetinfoba2/DrawingView.kt
+++ app/src/main/java/com/example/projetinfoba2/DrawingView.kt
@@ -1,68 +1,68 @@
 package com.example.projetinfoba2
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.SurfaceView
-import android.util.Log
+import android.view.WindowManager
 
 class DrawingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr) {
-    val ctx = getContext() //donne le contexte de l'activité ou du fragment où on veut dessiner l'oiseau
-
-    private val backgroundImage: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.run_background)
-    private val scaledBackgroundImage: Bitmap = Bitmap.createScaledBitmap(backgroundImage, 10000, 1400, true)
+    val ctx = getContext() //donne le contexte de l'activité ou du fragment où on veut dessiner
+    var screenWidth = 0f// Largeur de l'écran en pixels
+    var screenHeight = 0f // Hauteur de l'écran en pixels
+    private val backgroundImage: Bitmap =
+        BitmapFactory.decodeResource(resources, R.drawable.run_background)
+    private val scaledBackgroundImage: Bitmap =
+        Bitmap.createScaledBitmap(backgroundImage, 10000, 1400, true)
     private val backgroundspeed = 5f
     var backgroundOffset = 0f
 
+    init {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val displayMetrics = DisplayMetrics()
+        display.getMetrics(displayMetrics)
+        screenWidth = displayMetrics.widthPixels.toFloat()
+        screenHeight = displayMetrics.heightPixels.toFloat()
+    }
 
 
+    val joueur = Joueur(ctx, 0f, screenHeight / 2)
 
-    val joueur = Joueur(ctx,1000f,1000f)
-
-    val projectileList1 = mutableListOf<Projectile>()
-    val projectileList2 = mutableListOf<Projectile>()
+    val projectileList = mutableListOf<Projectile>()
+    val projectileToRemove = mutableListOf<Projectile>()
 
     var oiseauList = mutableListOf<Oiseau>()
     var oiseauToRemove = mutableListOf<Oiseau>()
     var lastOiseauTime = 0L
 
-    var obstacleList =  mutableListOf<Obstacle>()
+    var obstacleList = mutableListOf<Obstacle>()
     var obstacleToRemove = mutableListOf<Obstacle>()
     var lastObstacleTime = 0L
-
-    val boutonList = mutableListOf<Bouton>()
-
-    val bouton0 = Bouton(ctx, 100f, 1000f, 0)
-    val bouton1 = Bouton(ctx, 500f, 1000f, 1)
-    val bouton2 = Bouton(ctx, 300f, 800f, 2)
 
     val boutonTir = Bouton(ctx, 1500f, 800f, 0)
 
     val joystick = Joystick(300f,750f, 80f,190f)
 
-    init {
-        boutonList.add(bouton0)
-        boutonList.add(bouton1)
-        boutonList.add(bouton2)
-    }
-
-    fun generateObstacle(x: Float, y: Float) {
-        val obstacle = Obstacle(context, x, y)
-        obstacleList.add(obstacle)
-    }
-
-    fun generateOiseau(x: Float, y: Float) {
-        val oiseau = Oiseau(context, x, y)
-        oiseauList.add(oiseau)
-    }
 
 
     override fun onDraw(canvas: Canvas?) {
-        //super.onDraw(canvas)
+
+        // super.onDraw(canvas)
+
         canvas?.drawColor(Color.BLACK)// Couleur de fond de la Drawingview
 
-        canvas?.drawBitmap(scaledBackgroundImage, backgroundOffset, 0f, null) // deplace l'image d'arriere plan
+        canvas?.drawBitmap(
+            scaledBackgroundImage,
+            backgroundOffset,
+            0f,
+            null
+        ) // deplace l'image d'arriere plan
         backgroundOffset -= backgroundspeed//
         if (backgroundOffset < -7000f) { //
             backgroundOffset = 0f
@@ -72,29 +72,26 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
 
             joystick.draw(canvas)
             joueur.draw(canvas)
-            //joueur.gravite()
-            for (bouton in boutonList){
-                bouton.draw(canvas)
-            }
-            // Génère des obstacles toutes les 5 secondes
 
+            // Génère des obstacles toutes les 5 secondes
             if (System.currentTimeMillis() - lastObstacleTime > 5000) {
                 lastObstacleTime = System.currentTimeMillis()
-                generateObstacle(3000f, 1200f)
+                val obstacle = Obstacle(context, x, y)
+                obstacleList.add(obstacle)
             }
 
             // Génère des oiseaux toutes les 10 secondes
 
             if (System.currentTimeMillis() - lastOiseauTime > 10000) {
                 lastOiseauTime = System.currentTimeMillis()
-                generateOiseau(100f, 100f)
+                val oiseau = Oiseau(context, x, y)
+                oiseauList.add(oiseau)
             }
 
             // Dessine tous les obstacles existants
                 for (obstacle in obstacleList) {
                     obstacle.draw(canvas)
                     obstacle.updatePosition()
-
                     // Vérifie si un obstacle est sorti de l'écran et le supprime de la liste
                     if (obstacle.obstaclePosition.right < 0) {
                         obstacleToRemove.add(obstacle)
@@ -107,10 +104,17 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
                 oiseau.updatePosition()
 
                 //fais tomber un oeuf si l'oiseau est au dessus du joueur
-                if (oiseau.oiseauposition.left == joueur.joueurPosition.left){
+                if (oiseau.oiseauposition.left == joueur.joueurPosition.left) {
                     val x = oiseau.oiseauposition.left
                     val y = oiseau.oiseauposition.bottom
-                    projectileList1.add(Projectile(ctx, x, y,1)) // ajouter une nouvelle balle a la liste des ablles deja existantes
+                    projectileList.add(
+                        Projectile(
+                            ctx,
+                            x,
+                            y,
+                            1
+                        )
+                    ) // ajouter une nouvelle balle a la liste des ablles deja existantes
                 }
                 // Vérifie si un obstacle est sorti de l'écran et le supprime de la liste
                 if (oiseau.oiseauposition.left > 5000) {
@@ -118,14 +122,11 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
                 }
             }
 
-            for (projetile  in projectileList1) {
+            for (projetile in projectileList) {
                 projetile.draw(canvas)
-                projetile.updatePositionOeuf()
+                projetile.updatePositionBalle()
             }
-            for (projectile in projectileList2) {
-                projectile.draw(canvas)
-                projectile.updatePositionBalle()
-            }
+
 
 
             oiseauList.removeAll(oiseauToRemove)
@@ -144,28 +145,11 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
         val y = event.y
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                for (i in 0 until boutonList.size)
-                    if (boutonList[i].isClicked(x,y)){
-                       //if (i==0){joueur.marcheArrier()}
-                       //if(i==1){joueur.marcheAvant()}
-                       //if (i==2){
-                       //    joueur.saute()}
-                    }
-                //bird.updatePosition(x, y)
-                //if (bird.isClicked(x, y)) { // L'utilisateur a cliqué sur l'oiseau
-                //    projectileList1.add(Projectile(ctx, x, y,1)) // ajouter une nouvelle balle a la liste des ablles deja existantes
-                //    invalidate() // Actualiser la vue
-                //    return true
-                //}
 
                 if (joueur.isTouched(x,y)){
-                    projectileList2.add(Projectile(ctx,x,y,0))
+                    projectileList.add(Projectile(ctx, x, y, 0))
                     invalidate()
                 }
-                //else{
-                //    bird.updatePosition(x, y) // mettre a jour la poosition de l'oiseau
-                //    invalidate() // Actualiser la vue
-                //}
 
                 if(joystick.isPressed(x,y)){
                     joystick.setIsPressed(true)
--- app/src/main/java/com/example/projetinfoba2/Obstacle.kt
+++ app/src/main/java/com/example/projetinfoba2/Obstacle.kt
@@ -5,16 +5,24 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.RectF
-import android.os.Handler
 import java.util.*
 
 class Obstacle (private val context: Context, private val x: Float, private val y: Float) {
 
-    private val listeObstacleImage = intArrayOf(R.drawable.par_1,R.drawable.par_2,R.drawable.par_3,R.drawable.par_4 ,R.drawable.par_5,R.drawable.par_6
-        ,R.drawable.par_7,R.drawable.par_8,R.drawable.par_9,R.drawable.par_10) // liste des images des balles
+    private val listeObstacleImage = intArrayOf(
+        R.drawable.par_1,
+        R.drawable.par_2,
+        R.drawable.par_3,
+        R.drawable.par_4,
+        R.drawable.par_5,
+        R.drawable.par_6,
+        R.drawable.par_7,
+        R.drawable.par_8,
+        R.drawable.par_9,
+        R.drawable.par_10
+    ) // liste des images des balles
     private val obstacleTaille = 100f
-    val obstacleImage : Bitmap
-
+    private val obstacleImage: Bitmap
     private val obstacleVitesse = -5f // la vitesse eb x à laquelle la balle va se deplacer
     private val obstacleDegats = 100
     private var obstacleOnScreen = true
@@ -23,7 +31,6 @@ class Obstacle (private val context: Context, private val x: Float, private val 
         val random = Random().nextInt(listeObstacleImage.size)
         obstacleImage = BitmapFactory.decodeResource(context.resources, listeObstacleImage[random])
     }
-
     val obstaclePosition = RectF(x, y, x + obstacleImage.width, y - obstacleImage.height) // encode la position de la balle dans un rectangle
 
     fun draw(canvas: Canvas) {
@@ -35,14 +42,11 @@ class Obstacle (private val context: Context, private val x: Float, private val 
                 null
             )
         }
-
     }
 
-    fun updatePosition() { // actualise la position d
-        if (obstacleOnScreen) {
-            obstaclePosition.left += obstacleVitesse
-            obstaclePosition.right += obstacleVitesse
-        }
+    fun updatePosition() { // actualise la position
+        obstaclePosition.left += obstacleVitesse
+        obstaclePosition.right += obstacleVitesse
     }
 
     fun obstacleDestroyed() {
